101618cea9398f66f0b31da41ffde401c3253692
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-09-04 15:33:38 -0600

    Solve problem 2
    Add code for problem 2

--- assignment 1/main.m
+++ assignment 1/main.m
@@ -1,6 +1,8 @@
 % Jonathon Pearson
 % Assignment 1
 
+clear;
+
 disp("-----Finish Solving Problem 1-----")
 pause
 % load image
@@ -14,7 +16,40 @@ title('RGB Original Image');
 
 disp("-----Finish Solving Problem 2-----")
 pause
-%TODO: Solve problem 2
+
+%get all of our matrices 
+pepperGrayIm = rgb2gray(pepperIm);
+pepperGrayImT = pepperGrayIm.';
+
+pepperGrayImH(1:256, :) = pepperGrayIm(257:512, :);
+pepperGrayImH(257:512, :) = pepperGrayIm(1:256, :);
+
+pepperGrayImF = fliplr(pepperGrayIm);
+
+%display images
+figure();
+
+%display pepperGrayIm in top left
+subplot(2,2,1);
+imshow(pepperGrayIm);
+title('pepperGrayIm');
+
+%display pepperGrayImT in top right
+subplot(2,2,2);
+imshow(pepperGrayImT);
+title('pepperGrayImT');
+
+%display pepperGrayImH in bottom left
+subplot(2,2,3);
+imshow(pepperGrayImH);
+title('pepperGrayImH');
+
+%display pepperGrayImF in bottom right
+subplot(2,2,4);
+imshow(pepperGrayImF);
+title('pepperGrayImF');
+
+
 
 disp("-----Finish Solving Problem 3-----")
 pause
