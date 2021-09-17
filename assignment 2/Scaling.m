aeeb4bb9a39ca8c061055a6a35c6c4cd23ad8064
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-09-16 20:25:11 -0600

    Update Scaling.m
    Add scaling function to file

--- assignment 2/Scaling.m
+++ assignment 2/Scaling.m
@@ -1,2 +1,10 @@
 % Jonathon Pearson
 % Assignment 2
+
+function [scaledIm, transFunc] = Scaling(inputIm, range)
+    %TODO: Scale image
+    %TODO: check for proper input
+    
+    scaledIm = inputIm;
+    transFunc =  range;
+end
