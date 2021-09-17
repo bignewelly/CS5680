8459558627ef5154264717798c5cd805defbdda9
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-09-16 20:17:23 -0600

    Initial assignment 2
    Add assignment 2 skeloton

---
+++ assignment 2/main.m
@@ -0,0 +1,43 @@
+% Jonathon Pearson
+% Assignment 2
+
+clear;
+
+% load image
+foodIm = imread('Food.jpg');
+
+% -----Problem 1-----
+% TODO solve problem 1
+
+figure();
+
+imshow(foodIm);
+title('RGB Original Image');
+
+disp("-----Finish Solving Problem 1-----")
+pause
+
+% -----Problem 2-----
+% TODO solve problem 2
+disp("-----Finish Solving Problem 2-----")
+pause
+
+% -----Problem 3-----
+% load image
+disp("-----Finish Solving Problem 3-----")
+pause
+
+% -----Problem 4-----
+% TODO solve problem 4
+disp("-----Finish Solving Problem 4-----")
+pause
+
+% -----Problem 5-----
+% TODO solve problem 5
+disp("-----Finish Solving Problem 5-----")
+pause
+
+clear;
+close all;
+
+
