60dd79187432a381f4262f121d29a1c93cb3eff1
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-09-18 16:08:52 -0600

    add Scaling function
    Add Scaling function with input checks

--- assignment 2/Scaling.m
+++ assignment 2/Scaling.m
@@ -3,8 +3,30 @@
 
 function [scaledIm, transFunc] = Scaling(inputIm, range)
     %TODO: Scale image
-    %TODO: check for proper input
+    if ~isinteger(range)
+        throw(MException('myComponent:inputError', 'Range values should be integers.'));
+    end
     
+    if ~(range >= 0)
+        throw(MException('myComponent:inputError','Range values should be positive.'));
+    end
+    
+    if range(1) > range(2)
+        throw(MException('myComponent:inputError','Second range value should be greater than first.'));
+    end
+    
+    oriMinIntensity = inputIm(2);
+    oriMaxIntensity = inputIm(3);
+    
+    slope = double(range(2) - range(1))/double(oriMaxIntensity - oriMinIntensity);
+    
+    transFunc = zeros(oriMaxIntensity - oriMinIntensity + 1);
+    
+    for (i = oriMinIntensity: oriMaxIntensity)
+    end
+   
+    
+    y = original * slope + oriMinIntensity
     scaledIm = inputIm;
     transFunc =  range;
 end
--- assignment 2/main.m
+++ assignment 2/main.m
@@ -6,20 +6,44 @@ clear;
 % load image
 foodIm = imread('Food.jpg');
 
+sizeOri = numel(foodIm);
+
+maxValue = 0;
+minValue = 255;
+totalValue = double(0);
+
+% Get maxValue, minValue, and totalValue
+for i = 1:sizeOri
+    if maxValue < foodIm(i)
+        maxValue = foodIm(i);
+    end
+
+    if minValue > foodIm(i)
+        minValue = foodIm(i);
+    end
+
+    totalValue = totalValue + double(foodIm(i));
+end
+
+% calculate meanValue
+meanValue = double(totalValue)/double(sizeOri);
+
 % -----Problem 1-----
 % TODO solve problem 1
 % Each of these should throw an exception:
-scaling(foodIm, [0.1, 1.2]);
-scaling(foodIm, [-255, -1]);
-scaling(foodIm, [255, 0]);
+Scaling(foodIm, [0.1, 1.2]);
+Scaling(foodIm, [-255, -1]);
+Scaling(foodIm, [255, 0]);
 
 % Rescale the image to have a good quality range
-[scaledFoodIm, transFunc] = scaling(foodIm, [0, 255]);
+[scaledFoodIm, transFunc] = Scaling([foodIm, minValue, maxValue], [0, 255]);
 
 figure();
 %TODO: show transFunc in graph
-imshow(scaledFoodIm);
-title('RGB Original Image');
+plot([minValue, maxValue], transFunc);
+
+xlabel('Original Range');
+ylabel('Scaled Range');
 
 disp("-----Finish Solving Problem 1-----")
 pause
