bf8c911f944bafad1a83ed2b01b295a99adff473
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-09-30 18:50:31 -0600

    Assignment 3
    Add basic files for assignment 3

---
+++ assignment 3/main.m
@@ -0,0 +1,36 @@
+% Jonathon Pearson
+% Assignment 3
+
+clear;
+
+% load image
+
+% -----Problem 1-----
+% TODO: Solve Problem 1
+disp("-----Finish Solving Problem 1-----")
+pause
+
+% -----Problem 2-----
+% TODO: Solve Problem 2
+disp("-----Finish Solving Problem 2-----")
+pause
+
+% -----Problem 3-----
+% TODO: Solve proglem 3
+disp("-----Finish Solving Problem 3-----")
+pause
+
+% -----Problem 4-----
+% TODO solve problem 4
+disp("-----Finish Solving Problem 4-----")
+pause
+
+% -----Problem 5-----
+% TODO solve problem 5
+disp("-----Finish Solving Problem 5-----")
+pause
+
+clear;
+close all;
+
+
