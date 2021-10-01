21ef92c459745d7469f25068cb4c87f32191d122
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-09-30 20:18:37 -0600

    Add input validation
    Add input validation and show the images after running processing function.

--- assignment 3/AverageFiltering.m
+++ assignment 3/AverageFiltering.m
@@ -1,3 +1,27 @@
 % Jonathon Pearson
 % Assignment 3
 
+function[filteredIm]= AverageFiltering(im, mask)
+  
+    if mask < 0
+        throw(MException('myComponent:inputError', 'All mask values should be positive.'));
+    end
+    
+    if ~(abs(1 - sum(mask, 'all')) < 0.0001)
+        throw(MException('myComponent:inputError','Mask values should add up to 1.'));
+    end
+    
+    [m, n] = size(mask);
+    for i = 1:m
+        for j = 1:(n-i+1)
+            if ~(mask(i,j) == mask(j,i))
+                throw(MException('myComponent:inputError','Mask must be symmetrical around the center.'));
+            end
+        end
+    end
+  
+    
+    %TODO: implement filtering function
+    
+    filteredIm = im;
+end
--- assignment 3/main.m
+++ assignment 3/main.m
@@ -7,6 +7,35 @@ clear;
 
 % -----Problem 1-----
 % TODO: Solve Problem 1
+
+threeByThree = double([1 1 1; 1 2 1; 1 1 1]);
+threeByThree = threeByThree/sum(threeByThree, 'all');
+fiveByFive = double([1 1 1 1 1; 1 1 1 1 1; 1 1 1 1 1; 1 1 1 1 1; 1 1 1 1 1]);
+fiveByFive = fiveByFive/sum(fiveByFive, 'all');
+
+circuitIm = imread('Circuit.jpg');
+
+avgFilt3X3CircuitIm = AverageFiltering(circuitIm, threeByThree);
+avgFilt5X5CircuitIm = AverageFiltering(circuitIm, fiveByFive);
+
+%display images
+figure();
+
+%display circuitIm on the left
+subplot(1,3,1);
+imshow(circuitIm);
+title('Original Circuit Image');
+
+%display avgFilt3X3CircuitIm in top right
+subplot(1,3,2);
+imshow(avgFilt3X3CircuitIm);
+title('3X3 Weighted Filter');
+
+%display avgFilt5X5CircuitIm in bottom left
+subplot(1,3,3);
+imshow(avgFilt5X5CircuitIm);
+title('5X5 Standard Filter');
+
 disp("-----Finish Solving Problem 1-----")
 pause
 
