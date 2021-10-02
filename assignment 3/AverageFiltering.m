a0925e53cd4520cb769ee6d56517c88833a5caf1
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-10-02 10:19:22 -0600

    Update AverageFiltering.m
    Implement filtering function.

--- assignment 3/AverageFiltering.m
+++ assignment 3/AverageFiltering.m
@@ -1,7 +1,7 @@
 % Jonathon Pearson
 % Assignment 3
 
-function[filteredIm]= AverageFiltering(im, mask)
+function[filteredIm]= AverageFiltering(im, mask)     
   
     if mask < 0
         throw(MException('myComponent:inputError', 'All mask values should be positive.'));
@@ -21,7 +21,23 @@ function[filteredIm]= AverageFiltering(im, mask)
     end
   
     
-    %TODO: implement filtering function
+    [padN, padM] = size(mask);
+    padN = floor(padN/2);
+    padM = floor(padM/2);
+    [imN, imM] = size(im);
+    paddedIm = zeros(imN + padN*2, imM + padM*2);
+    
+    paddedIm(padN + 1:padN+imN, padM + 1:padM+imM) = im;
+    
+    [m, n] = size(im);
     
     filteredIm = im;
+    
+    for i = padM + 1: padM + m
+        for j = padN + 1: padN + n
+            subset = paddedIm(i - padM: i + padM, j - padN: j + padN);
+            filteredIm(i - padN, j - padM) =uint8(sum(subset.* mask, 'all'));
+        end
+    end
+    
 end
