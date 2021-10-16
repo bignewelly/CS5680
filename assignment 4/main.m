e768ebcf4362c6b2b62a116958b202c2ce6d42b5
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-10-16 16:42:28 -0600

    Change to frequency domain and back
    Change image to frequency domain and back to test that I'm doing it properly.

--- assignment 4/main.m
+++ assignment 4/main.m
@@ -1,2 +1,83 @@
 % Jonathon Pearson
 % Assignment 4
+
+clear;
+
+% -----Problem I-----
+%TODO: Solve Problem I
+% Problem I - 1
+sampleIm = imread('Sample.jpg');
+
+cFSampleIm = fftshift(fft2(sampleIm));
+
+%TODO: apply Guassian low-pass filter
+gFilter = ones(size(sampleIm));
+gFilteredSampleIm = uint8(real(ifft2(ifftshift(cFSampleIm))));
+
+%TODO: apply butterworth hight-pass filter
+bWFilter = ones(size(sampleIm));
+bWFilteredSampleIm = uint8(real(ifft2(ifftshift(cFSampleIm))));
+
+%display images
+figure();
+
+%display sampleIm on the top
+subplot(2,1,1);
+imshow(sampleIm);
+title('Original Sample Image');
+
+%display gFilter in bottom left
+subplot(2,2,3);
+imshow(gFilter);
+title('Gaussian Filter');
+
+%display gFilteredSampleIm in bottom right
+subplot(2,2,4);
+imshow(gFilteredSampleIm);
+title('Filtered Image');
+
+%display images
+figure();
+
+%display sampleIm on the top
+subplot(2,1,1);
+imshow(sampleIm);
+title('Original Sample Image');
+
+%display bWFilter in bottom left
+subplot(2,2,3);
+imshow(bWFilter);
+title('Butterworth Filter');
+
+%display gFilteredSampleIm in bottom right
+subplot(2,2,4);
+imshow(bWFilteredSampleIm);
+title('Filtered Image');
+
+disp("-----Finish Solving Problem I-----")
+pause;
+
+% -----Problem II-----
+%TODO: Solve Problem II
+disp("-----Finish Solving Problem II-----")
+pause;
+
+% -----Problem III-----
+%TODO: Solve Problem III
+disp("-----Finish Solving Problem III-----")
+pause;
+
+% -----Problem IV-----
+%TODO: Solve Problem IV
+disp("-----Finish Solving Problem IV-----")
+pause;
+
+% -----Problem V-----
+%TODO: Solve Problem V
+disp("-----Finish Solving Problem V-----")
+pause;
+
+clear;
+close all;
+
+
