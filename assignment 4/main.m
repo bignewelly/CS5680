893cf42e0f42e8d01ba1e479cd99490c2c0b8e07
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-10-19 16:31:46 -0600

    Update main.m
    Simplify code

--- assignment 4/main.m
+++ assignment 4/main.m
@@ -89,60 +89,48 @@ fMagSampleIm = abs(cFSampleIm);
 fPhaseSampleIm = angle(cFSampleIm);
 
 % get maginitude and phase
-%call abs
 fMagCapitolIm = abs(cFCapitolIm);
-%call angle
 fPhaseCapitolIm = angle(cFCapitolIm);
-
-realMagSampleIM = real(log(fMagSampleIm));
-realPhaseSampleIm = real(fPhaseSampleIm);
-realMagCapitolIm = real(log(fMagCapitolIm));
-realPhaseCapitolIm = real(fPhaseCapitolIm);
-
-scaledFMagSampleIm =uint8(double(realMagSampleIM - min(realMagSampleIM, [], 'all'))./double(max(realMagSampleIM, [], 'all') - min(realMagSampleIM, [], 'all')).*255);
-scaledFPhaseSampleIm =uint8(double(realPhaseSampleIm - min(realPhaseSampleIm, [], 'all'))./double(max(realPhaseSampleIm, [], 'all') - min(realPhaseSampleIm, [], 'all')).*255);
-scaledFMagCapitolIm =uint8(double(realMagCapitolIm - min(realMagCapitolIm, [], 'all'))./double(max(realMagCapitolIm, [], 'all') - min(realMagCapitolIm, [], 'all')).*255);
-scaledFPhaseCapitolIm =uint8(double(realPhaseCapitolIm - min(realPhaseCapitolIm, [], 'all'))./double(max(realPhaseCapitolIm, [], 'all') - min(realPhaseCapitolIm, [], 'all')).*255);
 
 %display images
 figure();
 
 %display scaledFMagSampleIm on the top left
 subplot(2,2,1);
-imshow(scaledFMagSampleIm);
+imshow(log(fMagSampleIm), []);
 title('Maginitude of Sample.jpg');
 
 %display scaledFPhaseSampleIm in top right
 subplot(2,2,2);
-imshow(scaledFPhaseSampleIm);
+imshow(fPhaseSampleIm, []);
 title('Phase of Sample.jpg');
 
 %display scaledFMagCapitolIm on the top left
 subplot(2,2,3);
-imshow(scaledFMagCapitolIm);
+imshow(log(fMagCapitolIm), []);
 title('Maginitude of Capitol.jpg');
 
 %display scaledFPhaseCapitolIm in top right
 subplot(2,2,4);
-imshow(scaledFPhaseCapitolIm);
+imshow(fPhaseCapitolIm, []);
 title('Phase of Capitol.jpg');
 
 % -----Problem II-2-----
 % reconstruct images with switched magnitudes
-magSwitchCapitolIm = uint8(real(ifft2(ifftshift(fMagSampleIm .* exp(fPhaseCapitolIm.*1i)))));
-magSwitchSampleIm = uint8(real(ifft2(ifftshift(fMagCapitolIm .* exp(fPhaseSampleIm.*1i)))));
+magSwitchCapitolIm = ifft2(ifftshift(fMagSampleIm .* exp(fPhaseCapitolIm.*1i)));
+magSwitchSampleIm = ifft2(ifftshift(fMagCapitolIm .* exp(fPhaseSampleIm.*1i)));
 
 %display images
 figure();
 
 %display magSwitchCapitolIm on the left
 subplot(1,2,1);
-imshow(magSwitchCapitolIm);
+imshow(real(magSwitchCapitolIm), []);
 title('New Capitol Image');
 
 %display magSwitchSampleIm in top right
 subplot(1,2,2);
-imshow(magSwitchSampleIm);
+imshow(real(magSwitchSampleIm), []);
 title('New Sample Image');
 
 disp("-----Finish Solving Problem II-----")
