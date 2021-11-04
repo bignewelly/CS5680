2fdad95b5c10987178ab2a84c7c7cce333704ca8
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-11-03 19:25:25 -0600

    Initial files
    Add initial files for assignment 6

---
+++ assignment 6/main.m
@@ -0,0 +1,21 @@
+% Jonathon Pearson
+% Assignment 6
+
+clear;
+
+% -----Problem 1-----
+disp("-----Finish Solving Problem 1-----")
+pause;
+
+% -----Problem 2-----
+disp("-----Finish Solving Problem 2-----")
+pause;
+
+% -----Problem 3-----
+disp("-----Finish Solving Problem 3-----")
+pause;
+
+clear;
+close all;
+
+
