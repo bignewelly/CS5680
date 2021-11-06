f54351a1bc7d793c35911678619ef1322653d414
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-11-06 17:59:01 -0600

    Problem 3
    Solve problem 3.1 and 3.2

--- assignment 6/main.m
+++ assignment 6/main.m
@@ -179,10 +179,100 @@ disp("-----Finish Solving Problem 2-----")
 pause;
 
 % -----Problem 3-----
+% Problem 3.1
+lenaIm = imread('Lena.jpg');
+
+dwtmode('per');
+
+% docompose the image
+[c3LenaIm, s3LenaIm] = wavedec2(lenaIm, 3, 'db9');
+H = appcoef2(c3LenaIm,s3LenaIm,'db9',3);
+[h3,v3,d3] = detcoef2('all',c3LenaIm,s3LenaIm,3);
+[h2,v2,d2] = detcoef2('all',c3LenaIm,s3LenaIm,2);
+[h1,v1,d1] = detcoef2('all',c3LenaIm,s3LenaIm,1);
+
+rng(5832);
+
+b = logical(round(rand(1, numel(H))));
+
+[M, N] = size(H);
+beta = 30;
+
+for i=1:M
+    for j=1:N
+        currentB = b((j-1)*M + i);
+        
+        if currentB 
+            if mod(H(i,j), beta) >= 0.25*beta
+                H(i,j) = H(i,j) - mod(H(i,j), beta) + 0.75*beta;
+            else
+                H(i,j) = (H(i,j)-0.25*beta) - mod(H(i,j)-0.25*beta, beta) + 0.75*beta;
+            end
+        else
+            if mod(H(i,j), beta) <= 0.75*beta
+                H(i,j) = H(i,j) - mod(H(i,j), beta) + 0.25*beta;
+            else
+                H(i,j) = (H(i,j)-0.5*beta) - mod(H(i,j)-0.5*beta, beta) + 0.25*beta;
+            end
+        end
+        
+    end
+end
+
+% recompose image
+recApp2 = idwt2(H, h3,v3,d3, 'db9');
+recApp1 = idwt2(recApp2, h2,v2, d2, 'db9');
+recLenaIm = uint8(idwt2(recApp1, h1,v1,d1, 'db9'));
+
+% display images
+figure();
+
+%display lenaIm on the top
+subplot(2,1,1);
+imshow(lenaIm);
+title('Original');
+
+%display recLenaIm on the bottom left
+subplot(2,2,3);
+imshow(recLenaIm);
+title('Renconstructed Lena');
+
+%display difference on the bottom rigt
+subplot(2,2,4);
+imshow(lenaIm - recLenaIm, []);
+title('Difference');
+
+% Problem 3.2
+[c3RecLenaIm, s3RecLenaIm] = wavedec2(recLenaIm, 3, 'db9');
+newH = appcoef2(c3RecLenaIm,s3RecLenaIm,'db9',3);
+
+newB = logical(zeros(size(b)));
+
+[M, N] = size(newH);
+
+for i=1:M
+    for j=1:N
+        if mod(newH(i,j), beta) > beta/2
+            newB((j-1)*M + i) = 1;
+        end
+    end
+end
+
+if newB == b
+    disp('newB and b are identical');
+else
+    disp("newB and b aren't identical");
+    percentMatched = numel(find(newB == b))/numel(newB);
+    disp('percent matched:');
+    disp(percentMatched);
+end
 disp("-----Finish Solving Problem 3-----")
 pause;
 
 clear;
+
 close all;
+
+
 
 
