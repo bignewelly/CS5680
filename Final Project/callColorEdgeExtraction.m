83dd5c885d12ac2e033ebdff00a23d433b37df00
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-11-20 17:31:31 -0700

    Delete callColorEdgeExtraction.m
    Remove extra file.

--- Final Project/callColorEdgeExtraction.m
+++
@@ -1,7 +0,0 @@
-% Jonathon Pearson
-% Final Project
-
-function [edges] = CallColorEdgeExtraction(yuvIm, Ty, Tu, Tv)
-    % filter templates
-    edges = CallEdgeExtraction(yuvIm(:, :, 1), Ty) | CallEdgeExtraction(yuvIm(:, :, 2), Tu) | CallEdgeExtraction(yuvIm(:, :, 3), Tv);
-end
