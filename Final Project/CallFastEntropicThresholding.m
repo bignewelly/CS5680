edb6ddda8644a3e242279062884516c3147dfe67
Jon P <42878225+bignewelly@users.noreply.github.com>
2021-11-27 23:05:12 -0700

    Update Fast entropicThresholding
    Update code for fast entropic thresholding

--- Final Project/CallFastEntropicThresholding.m
+++ Final Project/CallFastEntropicThresholding.m
@@ -2,23 +2,34 @@
 % Final Project
 
 function [threshold] = CallFastEntropicThresholding(Im)
+    threshold = min(Im,[],'all');
+    f = numel(find(Im == threshold));        
+    pn = f/numel(find(Im <= threshold));
+    pe = f/numel(find(Im > threshold));
+
+    hn = - pn*log(pn);
+    he = - pe*log(pe); 
     
-    threshold = 0;
-    hT = 0;
-    for t=min(Im,[],'all'):max(Im,[],'all')
-        nonEdge = numel(find(Im <= t));
-        edge = numel(find(Im > t));
-        hnT = 0;
-        heT = 0;
-        for i=0:t
-            fi = numel(find(Im == i));
-            pn = fi/nonEdge;
-            pe = fi/edge;
-            
-            hnT = hnT - pn*log(pn);
-            heT = heT - pe*log(pe);            
-        end
-        ht = hnT + heT;
+    for i=threshold +1:max(Im,[],'all')
+        f = numel(find(Im == i));
+        if f
+            pe = f/numel(find(Im > threshold));
+            he = he - pe*log(pe);
+        end        
+    end
+    
+    hT = hn + he;
+    for t=threshold + 1:max(Im,[],'all')
+        p0 = numel(find(Im <= t - 1));
+        p1 = numel(find(Im > t - 1));
+        p0p = numel(find(Im <= t));
+        p1p = numel(find(Im > t));
+        ft = numel(find(Im == t));
+        
+        hn = p0/p0p*hn-ft/p0p*log(ft/p0p)-p0/p0p*log(p0/p0p);
+        he = p1/p1p*he-ft/p1p*log(ft/p1p)-p1/p1p*log(p1/p1p);
+        
+        ht = hn + he;
 
         if ht > hT
             hT = ht;
